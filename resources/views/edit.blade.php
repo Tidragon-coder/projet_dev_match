d412736a4842f1c8d32b998423f595c5a5fafb1e
clement <clementboscher6@gmail.com>
2025-04-01 00:10:47 +0200

    css-login-register-done-and-profile-not-totaly-done

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,7 +4,8 @@
     <div class="container mt-5">
         <h2 class="mb-4">Modifier le Profil</h2>
         
-        <form action="{{ route('update') }}" method="POST">
+        <form action="{{ route('update') }}" method="POST" enctype="multipart/form-data">
+
             @csrf
             
             <div class="mb-3">
@@ -54,6 +55,12 @@
             <div class="mb-3">
                 <label for="year_experience" class="form-label">Années d'expérience</label>
                 <input type="number" name="year_experience" id="year_experience" class="form-control" value="{{ old('year_experience', $user->year_experience) }}">
+            </div>
+            <div class="mb-3">
+            <label class="block w-full px-4 py-2 bg-slate-700 border border-slate-600 rounded-md text-white hover:bg-slate-600 cursor-pointer text-center">
+                <span class="text_choose_img">Choisir une image</span>
+                <input type="file" name="profile_picture" id="profile_picture" accept="image/*" class="hidden">
+            </label>
             </div>
 
             <button type="submit" class="btn btn-primary">Mettre à jour</button>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,11 +9,12 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="icon" type="image/png" href="{{ asset('images/logo2.png') }}">
 
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <!-- fonts  -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <script src="app.js"></script>
 
 </head>
 <body>
